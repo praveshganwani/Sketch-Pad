0a9fe428cc2f236f5e15ee54fb18d8afbc6affa8
praveshganwani <59963061+praveshganwani@users.noreply.github.com>
2021-07-11 20:04:14 +0530

    added load balancing and show names on canvas functionality

--- public/sketch.js
+++ public/sketch.js
@@ -1,12 +1,43 @@
 let socket;
 let color = '#FFF';
 let strokeWidth = 4;
+let username = '';
+let room_code = '';
+let save_button = document.getElementById('save-canvas');
+let take_attendance = document.getElementById('take-attendance');
+var http = new XMLHttpRequest();
+
+take_attendance.addEventListener('click', () => {
+  const rcode = select('#rcode');
+  room_code = rcode.elt.outerText
+    .replace('Room Code: ', '')
+    .replace('(', '')
+    .replace(')', '');
+  window.open('http://localhost:3000/attendance?room_code=' + room_code);
+  // var url = 'http://localhost:3000/attendance';
+  // var params = 'room_code=' + room_code;
+  // http.open('GET', url + '?' + params, true);
+  // http.onreadystatechange = function () {
+  //   if (http.readyState == 4 && http.status == 200) {
+  //     alert(http.responseText);
+  //   }
+  // };
+  // http.send(null);
+});
 
 function setup() {
   // Creating canvas
   const cv = createCanvas(800, 600);
   cv.position(600, 90);
   cv.background(0);
+  const rcode = select('#rcode');
+  room_code = rcode.elt.outerText
+    .replace('Room Code: ', '')
+    .replace('(', '')
+    .replace(')', '');
+  save_button.addEventListener('click', () => {
+    saveCanvas(cv, 'Canvas-' + room_code, 'jpg');
+  });
 
   // Start the socket connection
   socket = io.connect('http://localhost:3000');
@@ -16,6 +47,51 @@ function setup() {
     stroke(data.color);
     strokeWeight(data.strokeWidth);
     line(data.x, data.y, data.px, data.py);
+    // text(data.username, data.mouseX + 20, data.mouseY + 20);
+  });
+
+  socket.on('username', (data) => {
+    console.log('Here ', data.username);
+    textSize(16);
+    fill(500);
+    strokeWeight(1);
+    text(data.username, data.x + 20, data.y + 20);
+  });
+
+  socket.on('user_joined', (users) => {
+    const user = select('#user-name');
+    username = user.elt.outerText.replace('Welcome, ', '');
+    console.log('User Joined: ', users);
+    var parent_div = document.getElementById('users');
+    parent_div.innerHTML = '';
+    users.forEach((user) => {
+      var tag = document.createElement('div');
+      tag.className = 'mb-1';
+      if (username == user) {
+        tag.innerHTML = user + ' (You)';
+      } else {
+        tag.innerHTML = user;
+      }
+      parent_div.appendChild(tag);
+    });
+  });
+
+  socket.on('user_disconnected', (users) => {
+    console.log('User Disconnected: ', users);
+    var parent_div = document.getElementById('users');
+    parent_div.innerHTML = '';
+    users.forEach((user) => {
+      var tag = document.createElement('div');
+      tag.className = 'mb-1';
+      if (username == user) {
+        tag.innerHTML = user + ' (You)';
+      } else {
+        tag.innerHTML = user;
+      }
+      // var text = document.createTextNode(user);
+      // tag.appendChild(text);
+      parent_div.appendChild(tag);
+    });
   });
 
   // Getting our buttons and the holder through the p5.js dom
@@ -43,20 +119,48 @@ function setup() {
     const width = parseInt(stroke_width_picker.value());
     if (width > 0) strokeWidth = width;
   });
+
+  const clear_btn = select('#clear-canvas');
+  clear_btn.mousePressed(() => {
+    clear();
+    cv.background(0);
+  });
 }
 
 function mouseDragged() {
+  const user = select('#user-name');
+  const rcode = select('#rcode');
+  username = user.elt.outerText.replace('Welcome, ', '');
+  room_code = rcode.elt.outerText
+    .replace('Room Code: ', '')
+    .replace('(', '')
+    .replace(')', '');
   // Draw
   stroke(color);
   strokeWeight(strokeWidth);
   line(mouseX, mouseY, pmouseX, pmouseY);
-
   // Send the mouse coordinates
-  sendmouse(mouseX, mouseY, pmouseX, pmouseY);
+  sendmouse(mouseX, mouseY, pmouseX, pmouseY, username, room_code);
+}
+
+function mouseReleased() {
+  const user = select('#user-name');
+  const rcode = select('#rcode');
+  username = user.elt.outerText.replace('Welcome, ', '');
+  room_code = rcode.elt.outerText
+    .replace('Room Code: ', '')
+    .replace('(', '')
+    .replace(')', '');
+  textSize(16);
+  fill(500);
+  strokeWeight(1);
+  text(username, mouseX + 20, mouseY + 20);
+  // Send the mouse coordinates
+  sendusername(mouseX, mouseY, username, room_code);
 }
 
 // Sending data to the socket
-function sendmouse(x, y, pX, pY) {
+function sendmouse(x, y, pX, pY, username) {
   const data = {
     x: x,
     y: y,
@@ -64,7 +168,22 @@ function sendmouse(x, y, pX, pY) {
     py: pY,
     color: color,
     strokeWidth: strokeWidth,
+    username: username,
+    roomCode: room_code,
   };
 
   socket.emit('mouse', data);
+}
+
+function sendusername(x, y, username) {
+  const data = {
+    x: x,
+    y: y,
+    color: color,
+    strokeWidth: strokeWidth,
+    username: username,
+    roomCode: room_code,
+  };
+
+  socket.emit('username', data);
 }
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 let username = '';
+let room_code = '';
 const http = require('http');
 const express = require('express');
 const util = require('util');
 const app = express();
+const fs = require('fs');
+const path = require('path');
+
 app.use(express.static('public'));
 
 app.set('port', '3000');
@@ -50,8 +54,18 @@ io.sockets.on('connection', (socket) => {
     if (err) throw err;
     console.log('Socket ID updated');
   });
+  socket.join(room_code);
+  var sql = "SELECT * FROM USERS WHERE room_code = '" + room_code + "'";
+  var users = [];
+  con.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    result.forEach((element) => {
+      users.push(element.username);
+    });
+    io.in(room_code).emit('user_joined', users);
+  });
   socket.on('mouse', (data) => {
-    socket.broadcast.emit('mouse', data);
+    socket.broadcast.to(data.roomCode).emit('mouse', data);
     var sql =
       "UPDATE users SET color = '" +
       data.color +
@@ -62,16 +76,42 @@ io.sockets.on('connection', (socket) => {
       "'";
     con.query(sql, function (err, result) {
       if (err) throw err;
-      console.log('Color And Stroke Width Updated');
+      console.log('Color And Stroke Width Updated' + data.username);
     });
   });
-  socket.on('disconnect', () => console.log('Client has disconnected'));
+  socket.on('username', (data) => {
+    socket.broadcast.to(data.roomCode).emit('username', data);
+  });
+  socket.on('disconnect', () => {
+    console.log('Client has disconnected');
+    var sql =
+      "UPDATE users SET room_code = '" +
+      '' +
+      "' WHERE socket_id = '" +
+      socket.id +
+      "'";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log('User Disconnected');
+      var sql = "SELECT * FROM USERS WHERE room_code = '" + room_code + "'";
+      var users = [];
+      con.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        result.forEach((element) => {
+          users.push(element.username);
+        });
+        io.in(room_code).emit('user_disconnected', users);
+      });
+    });
+  });
 });
 
 server.listen('3000');
 
 app.post('/submit', function (req, res) {
   username = req.body.username;
+  var val = Math.floor(1000 + Math.random() * 9000);
+  room_code = val.toString();
   var color = '#FFFFFF';
   var width = 4;
   var sql = "SELECT * FROM USERS WHERE username = '" + username + "'";
@@ -80,12 +120,14 @@ app.post('/submit', function (req, res) {
       const results = await query(sql);
       if (results.length == 0) {
         sql =
-          "INSERT INTO users (username, color, stroke_width) VALUES ('" +
+          "INSERT INTO users (username, color, stroke_width, room_code) VALUES ('" +
           username +
           "', '" +
           color +
           "', '" +
           width +
+          "', '" +
+          room_code +
           "')";
         con.query(sql, function (err, result) {
           if (err) throw err;
@@ -93,43 +135,109 @@ app.post('/submit', function (req, res) {
         });
       } else {
         results.forEach((element) => {
-          console.log(element.color + ' INSIDE ' + element.stroke_width);
           color = element.color;
           width = element.stroke_width;
+          var sql =
+            "UPDATE users SET room_code = '" +
+            room_code +
+            "' WHERE username = '" +
+            username +
+            "'";
+          con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log('Room Code Updated');
+          });
         });
       }
       console.log(results);
     } finally {
-      // con.end();
       console.log(color + ' ' + width);
       res.render('sketchpad.html', {
         color: color,
         width: width,
         username: username,
+        roomCode: room_code,
       });
     }
   })();
-  // con.query(sql, function (err, results, field) {
-  //   if (err) throw err;
-  //   if (results.length == 0) {
-  //     sql =
-  //       "INSERT INTO users (username, color, stroke_width) VALUES ('" +
-  //       username +
-  //       "', '" +
-  //       color +
-  //       "', '" +
-  //       width +
-  //       "')";
-  //     con.query(sql, function (err, result) {
-  //       if (err) throw err;
-  //       console.log('New User Inserted');
-  //     });
-  //   } else {
-  //     results.forEach((element) => {
-  //       console.log(element.color + ' INSIDE ' + element.stroke_width);
-  //       color = element.color;
-  //       width = element.stroke_width;
-  //     });
-  //   }
-  // });
+});
+
+app.post('/join-room', function (req, res) {
+  username = req.body.username;
+  room_code = req.body.roomCode;
+  var color = '#FFFFFF';
+  var width = 4;
+  var sql = "SELECT * FROM USERS WHERE username = '" + username + "'";
+  (async () => {
+    try {
+      const results = await query(sql);
+      if (results.length == 0) {
+        sql =
+          "INSERT INTO users (username, color, stroke_width, room_code) VALUES ('" +
+          username +
+          "', '" +
+          color +
+          "', '" +
+          width +
+          "', '" +
+          room_code +
+          "')";
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log('New User Inserted');
+        });
+      } else {
+        results.forEach((element) => {
+          color = element.color;
+          width = element.stroke_width;
+          var sql =
+            "UPDATE users SET room_code = '" +
+            room_code +
+            "' WHERE username = '" +
+            username +
+            "'";
+          con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log('Room Code Updated');
+          });
+        });
+      }
+      console.log(results);
+    } finally {
+      console.log(color + ' ' + width);
+      res.render('sketchpad.html', {
+        color: color,
+        width: width,
+        username: username,
+        roomCode: room_code,
+      });
+    }
+  })();
+});
+
+app.get('/attendance', (req, res) => {
+  var room_id = req.query.room_code.toString();
+  console.log(room_id);
+  var sql = "SELECT * FROM USERS WHERE room_code = '" + room_id + "'";
+  var users = [];
+  (async () => {
+    const results = await query(sql);
+    results.forEach((element) => {
+      users.push(element.username);
+    });
+    const stream = fs.createWriteStream(
+      './uploads/attendance-' + room_id + '.txt'
+    );
+    stream.once('open', function (fd) {
+      users.forEach((element) => {
+        stream.write(element + '\n');
+      });
+      stream.end();
+    });
+    const file = `${__dirname}/uploads/attendance-` + room_id + `.txt`;
+    console.log(file);
+    res.download(
+      path.join(__dirname, './uploads/attendance-' + room_id + '.txt')
+    ); // Set disposition and send it.
+  })();
 });
